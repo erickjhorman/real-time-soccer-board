98dcbc6ca8f7b9b0282c066e1c4c04b1c7178521
Erick Romero <erjhrome@bancolombia.com.co>
2024-03-11 16:32:57 -0500

    add: Service Discovery Client annotation to Producer Module

--- producer/src/main/java/org/demo/producer/ProducerApplication.java
+++ producer/src/main/java/org/demo/producer/ProducerApplication.java
@@ -2,12 +2,16 @@ package org.demo.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
 
 }
+
+
